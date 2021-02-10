3d48f2b861849d90aebe85d6e088365de7810c06
pratik choudhari <pratik.choudhari89@gmail.com>
2021-02-10 19:53:37 +0530

    add rotating proxies

--- scrapera/text/reddit.py
+++ scrapera/text/reddit.py
@@ -33,10 +33,26 @@ class RedditPostScraper:
         """
         Returns JSON response fetched from passed session and URL
         """
-        proxy = random.choice(self.proxies) if self.proxies else None
-        async with session.get(url, headers=self.get_headers,  proxy=proxy, verify_ssl=False) as response:
-            json_data = await response.json()
-            return json_data
+        if self.proxies:
+            while True:
+                if not self.proxies:
+                    print("[INFO]: All proxies expired, continuing with current machine IP")
+                    break
+                proxy = random.choice(self.proxies) if self.proxies else None
+                try:
+                    async with session.get(url, headers=self.get_headers, proxy=proxy, timeout=3) as response:
+                        json_data = await response.json()
+                        return json_data
+                except (
+                            asyncio.TimeoutError, aiohttp.client.ClientProxyConnectionError,
+                            aiohttp.client.ClientHttpProxyError,
+                            aiohttp.client.ServerDisconnectedError, aiohttp.client.ClientOSError):
+                    self.proxies.remove(proxy)
+
+        else:
+            async with session.get(url, headers=self.get_headers) as response:
+                json_data = await response.json()
+                return json_data
 
     async def _get_comments(self, id):
         """
@@ -59,26 +75,27 @@ class RedditPostScraper:
         """
         async with aiohttp.ClientSession() as session:
             json_data = await self._fetch_response(session, self.search_url.format(self.topic, self.last_post_id))
-            if 'subreddits' in json_data:
-                subreddits = json_data['subreddits']
-                postIds = list(json_data['posts'].keys())
-                if not postIds:
-                    self.data_exhausted = True
-                    return None
+            if json_data:
+                if 'subreddits' in json_data:
+                    subreddits = json_data['subreddits']
+                    postIds = list(json_data['posts'].keys())
+                    if not postIds:
+                        self.data_exhausted = True
+                        return None
 
-                for postId in postIds:
-                    post = json_data['posts'].get(postId)
-                    if not post:
-                        continue
-                    author_id = post['belongsTo']['id']
-                    subreddit = subreddits.get(author_id)['displayText'] if post['belongsTo']['type'] == 'subreddit' else f'u/{post["author"]}'
-                    comments = await self._get_comments(postId) if self.comments_flag else {}
-                    self.posts_data[postId] = {'title': post.get("title"), 'numComments': post.get("numComments"),
-                                                'upVotes': post.get("score"), 'author': post.get("author"),
-                                                'subreddit': subreddit, 'isSponsored': post.get('isSponsored'),
-                                                'link': post.get("permalink"), 'comments': comments}
-                    self.post_count += 1
-                self.last_post_id = postIds[-1]
+                    for postId in postIds:
+                        post = json_data['posts'].get(postId)
+                        if not post:
+                            continue
+                        author_id = post['belongsTo']['id']
+                        subreddit = subreddits.get(author_id)['displayText'] if post['belongsTo']['type'] == 'subreddit' else f'u/{post["author"]}'
+                        comments = await self._get_comments(postId) if self.comments_flag else {}
+                        self.posts_data[postId] = {'title': post.get("title"), 'numComments': post.get("numComments"),
+                                                    'upVotes': post.get("score"), 'author': post.get("author"),
+                                                    'subreddit': subreddit, 'isSponsored': post.get('isSponsored'),
+                                                    'link': post.get("permalink"), 'comments': comments}
+                        self.post_count += 1
+                    self.last_post_id = postIds[-1]
 
     async def _make_requests(self, numposts=100, out_path=None):
         """
@@ -119,6 +136,7 @@ class RedditPostScraper:
             return False
         assert os.path.exists(out_path), f'{out_path} does not exist'
         assert type(proxies) == list, 'Pass proxies as a list of strictly http URLS'
+        self.proxies = proxies
         self.comments_flag = comments
         self.topic = topic.replace(" ", "+")
         print('[INFO]: Scraping starts')
